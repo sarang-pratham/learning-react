3fd8abd6d67a3cd1bf711c84e4091e6edce83cf5
Sarang Pratham <sarangpratham720@gmail.com>
2022-03-06 08:48:30 +0530

    svg and css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import {useEffect} from 'react'
+import './App.css'
+import searchIcon from './search.svg'
+
+const API_URL = ' http://www.omdbapi.com/?apikey=63a5ac4c'
 
 const App = ()=>{
+    const SearchMovies = async (title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json()
+        console.log(data);
+    }
+
+
+    useEffect(()=>{
+        SearchMovies('spiderman')
+    },[])
+
     return (
         <h1>App</h1>
     )
